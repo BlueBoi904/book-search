75a8f67620e302b8d03290e2d9d5742aba06bbad
BlueBoi904 <cliff.mirschel@gmail.com>
2019-09-13 15:21:08 -0400

    Got data to display

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const BookCard = ({book}) => {
-    console.log(book)
+const BookCard = ({book, showModal}) => {
     return (
-        <div className='book-card'>
-            <p>book</p>
+        <div className='book-card' onClick={()=> showModal(book)}
+        >
+            {book.volumeInfo.title ? <p>{book.volumeInfo.title}</p> : null}
+            {book.volumeInfo.imageLinks ? <img alt={book.volumeInfo.title} src={book.volumeInfo.imageLinks.smallThumbnail} /> : null}
         </div>
     )
 }
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import BookCard from './BookCard';
+import "antd/dist/antd.css";
+import { Modal} from "antd";
+
 
 
 
 const BookList = ({books}) => {
-    const book = books.map(book => {
+    const [visible, setVisible] = useState(false);
+    const [popupData, setPopupData] = useState({});
 
-         return <a key={book.id} href={book.volumeInfo.canonicalVolumeLink}> <BookCard book={book}/></a>
+    const showModal = (bookData) => {
+        setVisible(true)
+        setPopupData({
+            image: bookData.volumeInfo.imageLinks.smallThumbnail
         })
+      };
+    
+     const handleOk = e => {
+        setVisible(false)
+      };
+    
+      const handleCancel = e => {
+        console.log(e);
+        setVisible(false)
+      };
+
+    const book = books.map((book) => {
+
+         return <BookCard key={book.etag} book={book} showModal={showModal}/>
+        })
+
+
     return (
+        
         <div className='book-list'>
+            <div>
             {book}
+            </div>
+            <Modal
+              title="Basic Modal"
+              visible={visible}
+              onOk={handleOk}
+              onCancel={handleCancel}
+            >
+              <img
+                alt="example"
+                src={popupData.image}
+                style={{ width: 200, height: 275 }}
+              />
+            </Modal>
         </div>
     )
 }
