d5ad4a6f0a567722e4fb2112a3618c963e73146d
BlueBoi904 <cliff.mirschel@gmail.com>
2019-09-13 09:17:46 -0400

    Added background vh and color

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header'
 import Books from './components/Books'
+import SearchBar from './components/SearchBar'
 
 function App() {
   return (
-    <div>
+    <div className='container'>
       <Header/>
+      <SearchBar />
       <Books/>
     </div>
   );
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import SearchBar from './SearchBar'
 
 export default function Books() {
     return (
         <div>
-            <SearchBar/>
+            Hello
         </div>
     )
 }
