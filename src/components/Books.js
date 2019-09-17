76848c4c04325dec5b8b47519c07873311b77f1b
BlueBoi904 <cliff.mirschel@gmail.com>
2019-09-17 15:03:11 -0400

    Fixed scrolling background color

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import 'antd/dist/antd.css';
-import { Spin } from 'antd';
-import { Alert } from "antd";
+import { Spin , Alert } from 'antd';
+
 import BookList from './BookList';
 import SearchBar from './SearchBar';
 
@@ -11,32 +11,32 @@ const Books = () => {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('')
+  const [errorMessage, setErrorMessage] = useState('');
 
   const URL = 'https://www.googleapis.com/books/v1/volumes?q=';
   const KEY = process.env.REACT_APP_API_KEY;
 
-  const onClose = e => {
-   setError(false);
+  const onClose = (e) => {
+    setError(false);
   };
 
   const handleSearch = async () => {
-     setBooks([]);
-     setLoading(true);
-    try {  
+    setBooks([]);
+    setLoading(true);
+    try {
       setError(false);
       if (searchField === '') {
         setLoading(false);
-        let errorMessage =  { message : 'You must enter a search value' };
+        const errorMessage = { message: 'You must enter a search value' };
         throw errorMessage;
       }
-      
+
       const response = await axios.get(
-        `${URL}${searchField}&maxResults=20&key=${KEY}`,
+        `${URL}${searchField}&key=${KEY}`,
       );
-      if (!response.data.items){
+      if (!response.data.items) {
         setLoading(false);
-        let errorMessage =  { message : 'There are no results for your search' };
+        const errorMessage = { message: 'There are no results for your search' };
         throw errorMessage;
       }
       setLoading(false);
@@ -55,24 +55,26 @@ const Books = () => {
   return (
     <div className="books">
       <SearchBar handleChange={handleChange} handleSearch={handleSearch} />
-      {loading && <div className="spinner">
+      {loading && (
+<div className="spinner">
        <Spin />
-      </div>}
+      </div>
+)}
       {error ? (
-      <div>
-      <Alert 
-        message='An error has occured'
+        <div>
+        <Alert
+        message="An error has occured"
         description={errorMessage}
         type="error"
         closable
-        onClose={onClose}/>
-        </div>
+        onClose={onClose} 
+      />
+      </div>
       ) : (
-      <BookList books={books} />
+        <BookList books={books} />
       )}
     </div>
   );
 };
 
 export default Books;
-
