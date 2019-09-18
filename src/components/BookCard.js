0fabef44d6b977bd08d61c5c7ed9eb859d4db76b
Benji Mirschel <cliff.mirschel@gmail.com>
2019-09-17 21:45:10 -0400

    Adding card styles

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import 'antd/dist/antd.css';
+// import { Card, Button } from 'antd';
 
 const BookCard = ({ book, showModal }) => (
   <div
@@ -16,6 +18,24 @@ const BookCard = ({ book, showModal }) => (
       />
     ) : null}
   </div>
+  // <div>
+  //   <Card
+  //     title={book.volumeInfo.title ? <p>{book.volumeInfo.title}</p> : null}
+  //     style={{ width: 300 }}
+  //   >
+  //     {book.volumeInfo.imageLinks ? (
+  //       <img
+  //         alt={book.volumeInfo.title}
+  //         src={book.volumeInfo.imageLinks.smallThumbnail}
+  //       />
+  //     ) : null}
+  //     <Button type="primary" size="small">
+  //       {' '}
+  //       Primary
+  //     </Button>
+  //     <p>Card content</p>
+  //   </Card>
+  // </div>
 );
 
 export default BookCard;
